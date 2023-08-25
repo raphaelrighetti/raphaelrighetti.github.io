53fdb623167f598a094c4ab0b5ca98aae8d5ea37
raphaelrighetti <raphaelrighettiramos@hotmail.com>
2023-08-25 03:01:33 -0300

    adicionando sessão contato

---
+++ js/copyEmailToClipboard.js
@@ -0,0 +1,6 @@
+const copyButton = document.querySelector('[data-js="copy-button"]');
+const email = document.querySelector('[data-js="email-p"]').textContent;
+
+copyButton.addEventListener('click', () => {
+  navigator.clipboard.writeText(email);
+});
